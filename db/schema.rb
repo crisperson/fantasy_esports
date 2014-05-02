fcaec05ee51273dd9f06702b226e6e989f7d9a46
Michael Hayes <michaeldothayes@gmail.com>
2014-05-02 10:06:33 -0400

    Migrate SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140502033441) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "aka"
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20140502033441) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["aka"], name: "index_users_on_aka", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["aka"], name: "index_users_on_aka", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
